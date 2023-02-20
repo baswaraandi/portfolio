f90df86c691a5fa343d8fd29744d46f817689f99
baswaraandi <baswaraandi2@gmail.com>
2023-02-21 00:01:49 +0700

    commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,15 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const title = document.querySelector('#titlePort');
+    const navMenu = document.querySelector('#navMenu');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        title.classList.add('title-color');
     }else{
         header.classList.remove('navbar-fixed');
+        title.classList.remove('title-color');
     }
 }
 
